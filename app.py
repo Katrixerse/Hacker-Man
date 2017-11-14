a4f43a2b26a1317bcf67ef9daa238c4ae1e69758
Zachary J. Myers <zmyers550@gmail.com>
2017-11-14 11:34:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+import discord
 
+client = discord.client
+
+prefix = "?"
+
+@client.event
+async def on_ready():
+    print('Ready!')
+    
+@client.event
+async def on_message(message):
+    if message.content.startswith(prefix + 'ping'):
+       await client.send_message(message.channel, 'Pong!')
+
+client.run('token')
