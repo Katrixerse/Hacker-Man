d0e1f2004a0db0ebb8997510a741b78dce87d93a
Zachary J. Myers <zmyers550@gmail.com>
2017-11-14 11:39:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,12 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith(prefix + 'ping'):
        await client.send_message(message.channel, 'Pong!')
+    
+    if message.content.startswith(prefix + 'prefix'):
+       await client.send_message(message.channel, 'Current prefix is: ' + prefix)
+    
+    if message.content.startswith(prefix + 'flipcoin'):
+       cointoflip = random.choice({"Heads!", "Tails!"})
+       await client.send_message(message.channel, 'The coin has been flipped and landed on' + cointoflip)
 
 client.run('token')
