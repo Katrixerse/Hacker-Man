7e62ae70c1727a0f86676d8d827b24b703f59982
Zachary J. Myers <zmyers550@gmail.com>
2017-11-14 11:45:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,9 @@ async def on_message(message):
     if message.content.startswith(prefix + 'flipcoin'):
        cointoflip = random.choice({"Heads!", "Tails!"})
        await client.send_message(message.channel, 'The coin has been flipped and landed on' + cointoflip)
-
+        
+    if message.content.startswith(prefix + '8ball'):
+       cointoflip = random.choice({"It is certain.", "No doubt about it.", "No chance."})
+       await client.send_message(message.channel, 'The coin has been flipped and landed on' + cointoflip)
+        
 client.run('token')
